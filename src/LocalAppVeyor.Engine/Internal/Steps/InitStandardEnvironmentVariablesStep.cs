fda138276a43cce2b696b083c62f08b75384054c
Joseph Benden <joe@benden.us>
2018-07-07 12:56:58 -0700

    Set environment variables for the user too.

--- src/LocalAppVeyor.Engine/Internal/Steps/InitStandardEnvironmentVariablesStep.cs
+++ src/LocalAppVeyor.Engine/Internal/Steps/InitStandardEnvironmentVariablesStep.cs
@@ -9,27 +9,36 @@ namespace LocalAppVeyor.Engine.Internal.Steps
         {
             executionContext.Outputter.Write("Initializing environment variables...");
 
-            Environment.SetEnvironmentVariable("APPVEYOR_BUILD_NUMBER", "0");
-            Environment.SetEnvironmentVariable("APPVEYOR_BUILD_VERSION", executionContext.BuildConfiguration.Version);
-            Environment.SetEnvironmentVariable("APPVEYOR_BUILD_FOLDER", executionContext.CloneDirectory);
-            Environment.SetEnvironmentVariable("CI", "False");
-            Environment.SetEnvironmentVariable("APPVEYOR", "False");
+            Environment.SetEnvironmentVariable("APPVEYOR_BUILD_NUMBER", "0", EnvironmentVariableTarget.User);
+            Environment.SetEnvironmentVariable("APPVEYOR_BUILD_VERSION", executionContext.BuildConfiguration.Version, EnvironmentVariableTarget.User);
+            Environment.SetEnvironmentVariable("APPVEYOR_BUILD_FOLDER", executionContext.CloneDirectory, EnvironmentVariableTarget.User);
+            Environment.SetEnvironmentVariable("CI", "False", EnvironmentVariableTarget.User);
+            Environment.SetEnvironmentVariable("APPVEYOR", "False", EnvironmentVariableTarget.User);
+
+            Environment.SetEnvironmentVariable("APPVEYOR_BUILD_NUMBER", "0", EnvironmentVariableTarget.Process);
+            Environment.SetEnvironmentVariable("APPVEYOR_BUILD_VERSION", executionContext.BuildConfiguration.Version, EnvironmentVariableTarget.Process);
+            Environment.SetEnvironmentVariable("APPVEYOR_BUILD_FOLDER", executionContext.CloneDirectory, EnvironmentVariableTarget.Process);
+            Environment.SetEnvironmentVariable("CI", "False", EnvironmentVariableTarget.Process);
+            Environment.SetEnvironmentVariable("APPVEYOR", "False", EnvironmentVariableTarget.Process);
 
             if (!string.IsNullOrEmpty(executionContext.CurrentJob.Configuration))
             {
-                Environment.SetEnvironmentVariable("CONFIGURATION", executionContext.CurrentJob.Configuration);
+                Environment.SetEnvironmentVariable("CONFIGURATION", executionContext.CurrentJob.Configuration, EnvironmentVariableTarget.User);
+                Environment.SetEnvironmentVariable("CONFIGURATION", executionContext.CurrentJob.Configuration, EnvironmentVariableTarget.Process);
             }
 
             if (!string.IsNullOrEmpty(executionContext.CurrentJob.Platform))
             {
-                Environment.SetEnvironmentVariable("PLATFORM", executionContext.CurrentJob.Platform);
+                Environment.SetEnvironmentVariable("PLATFORM", executionContext.CurrentJob.Platform, EnvironmentVariableTarget.User);
+                Environment.SetEnvironmentVariable("PLATFORM", executionContext.CurrentJob.Platform, EnvironmentVariableTarget.Process);
             }
 
             foreach (
                 var variable
                 in executionContext.BuildConfiguration.EnvironmentVariables.CommonVariables.Concat(executionContext.CurrentJob.Variables))
             {
-                Environment.SetEnvironmentVariable(variable.Name, variable.Value);
+                Environment.SetEnvironmentVariable(variable.Name, variable.Value, EnvironmentVariableTarget.User);
+                Environment.SetEnvironmentVariable(variable.Name, variable.Value, EnvironmentVariableTarget.Process);
                 }
 
             executionContext.Outputter.Write("Environment variables initialized.");
